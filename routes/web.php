b2fade502357eb3145ab59034385c7556c69d5bc
Dylan Ngo <it.tinhngo@gmail.com>
2020-04-05 17:06:33 +0700

    fix artsan optimize

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('task/index', 'TaskController@index');
 Route::get('task/create', 'TaskController@create');
